c90054494a0e2220bb1fb03eefedc3ce1dc7d203
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-09-23 17:01:45 +0800

    Add new p element

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,6 +27,7 @@ export default function Home({ allPostsData }) {
 					Hello, I'm Wyn Christian! I'm a data scientist and a web
 					developer. Lets goooo!
 				</p>
+				<p>Nice to meet you!</p>
 				<p>
 					(This is a sample website - you’ll be building a site like this
 					on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
